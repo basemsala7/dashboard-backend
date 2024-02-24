14abbcdc0074fc5c5d37c37dc00056fa326e0c38
basemsala7 <basembebo809@github.com>
2024-02-24 03:12:10 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,708 @@
+export const topdeals = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    username: "Elva McDonald",
+    email: "elva@gmail.com",
+    amount: "3.668",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Linnie Nelson",
+    email: "linnie@gmail.com",
+    amount: "3.256",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Brent Reeves",
+    email: "brent@gmail.com",
+    amount: "2.998",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Adeline Watson",
+    email: "adeline@gmail.com",
+    amount: "2.512",
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Juan Harrington",
+    email: "juan@gmail.com",
+    amount: "2.134",
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Augusta McGee",
+    email: "augusta@gmail.com",
+    amount: "1.932",
+  },
+  {
+    id: 7,
+    img: "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Angel Thomas",
+    email: "angel@gmail.com",
+    amount: "1.560",
+  },
+];
+
+export const users = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    lastName: "Hubbard",
+    firstName: "Eula",
+    email: "hubbard@gmail.com",
+    phone: "423 452 729",
+    createdAt: "05.07.2023",
+    verified: true,
+    activities: [
+      {
+        text: `Eula purchased Playstation 5 Digital Edition`,
+        time: "3 day ago",
+      },
+      {
+        text: `Eula  added 3 items into their wishlist`,
+        time: "1 week ago",
+      },
+      {
+        text: `Eula  purchased Sony Bravia KD-32w800`,
+        time: "2 weeks ago",
+      },
+      {
+        text: `Eula  reviewed a product`,
+        time: "1 month ago",
+      },
+      {
+        text: `Eula  added 1 items into their wishlist`,
+        time: "1 month ago",
+      },
+      {
+        text: `Eula  reviewed a product`,
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Manning",
+    firstName: "Stella",
+    email: "manning@gmail.com",
+    phone: "422 426 715",
+    createdAt: "03.07.2023",
+    verified: true,
+    activities: [
+      {
+        text: "Stella purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Stella added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Stella purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Stella reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Stella added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Stella reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Greer",
+    firstName: "Mary",
+    email: "greer@hottmail.com",
+    phone: "563 632 325",
+    createdAt: "02.07.2023",
+    verified: true,
+    activities: [
+      {
+        text: "Mary purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Mary added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Mary purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Mary reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Mary added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Mary reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Williamson",
+    firstName: "Mildred",
+    email: "williamson@gmail.com",
+    phone: "534 522 125",
+    createdAt: "12.06.2023",
+    verified: true,
+    activities: [
+      {
+        text: "Mildred purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Mildred added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Mildred purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Mildred reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Mildred added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Mildred reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/1758144/pexels-photo-1758144.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Gross",
+    firstName: "Jose",
+    email: "gobtagbes@yahoo.com",
+    phone: "462 252 624",
+    createdAt: "10.06.2023",
+    activities: [
+      {
+        text: "Jose purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Jose added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Jose purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Jose reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Jose added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Jose reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/769745/pexels-photo-769745.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Sharp",
+    firstName: "Jeremy",
+    email: "vulca.eder@mail.com",
+    phone: "735 523 563",
+    createdAt: "11.05.2023",
+    verified: true,
+    activities: [
+      {
+        text: "Jeremy purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Jeremy added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Jeremy purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Jeremy reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Jeremy added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Jeremy reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 7,
+    img: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Lowe",
+    firstName: "Christina",
+    email: "reso.bilic@gmail.com",
+    phone: "235 734 574",
+    createdAt: "04.05.2023",
+    activities: [
+      {
+        text: "Christina purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Christina added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Christina purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Christina reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Christina added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Christina reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 8,
+    img: "https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Dean",
+    firstName: "Garrett",
+    email: "codaic@mail.com",
+    phone: "377 346 834",
+    createdAt: "08.04.2023",
+    verified: true,
+    activities: [
+      {
+        text: "Garrett purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Garrett added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Garrett purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Garrett reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Garrett added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Garrett reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 9,
+    img: "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Parsons",
+    firstName: "Leah",
+    email: "parsons@gmail.com",
+    phone: "745 677 345",
+    createdAt: "04.04.2023",
+    activities: [
+      {
+        text: "Leah purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Leah added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Leah purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Leah reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Leah added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Leah reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 10,
+    img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Reid",
+    firstName: "Elnora",
+    email: "elnora@gmail.com",
+    phone: "763 345 346",
+    createdAt: "01.04.2023",
+    verified: true,
+    activities: [
+      {
+        text: "Elnora purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Elnora added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Gertrude purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Elnora reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Elnora added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Elnora reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 11,
+    img: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Dunn",
+    firstName: "Gertrude",
+    email: "gertrude@gmail.com",
+    phone: "124 456 789",
+    createdAt: "05.04.2023",
+    verified: true,
+    activities: [
+      {
+        text: "Gertrude purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Gertrude added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Gertrude purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Gertrude reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Gertrude added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Gertrude reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 12,
+    img: "https://images.pexels.com/photos/774095/pexels-photo-774095.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Williams",
+    firstName: "Mark",
+    email: "williams@hotmail.com",
+    phone: "626 235 345",
+    createdAt: "01.03.2023",
+    activities: [
+      {
+        text: "Mark purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Mark added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Mark purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Mark reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Mark added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Mark reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 13,
+    img: "https://images.pexels.com/photos/761977/pexels-photo-761977.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Cruz",
+    firstName: "Charlotte",
+    email: "charlotte@gmail.com",
+    phone: "673 547 343",
+    createdAt: "03.02.2023",
+    activities: [
+      {
+        text: "Charlotte purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Charlotte added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Charlotte purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Charlotte reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Charlotte added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Charlotte reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 14,
+    img: "https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    lastName: "Harper",
+    firstName: "Sara",
+    email: "harper@hotmail.com",
+    phone: "734 234 234",
+    createdAt: "01.02.2023",
+    activities: [
+      {
+        text: "Sara purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Sara added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Sara purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Sara reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Sara added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Sara reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+  {
+    id: 15,
+    img: "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    lastName: "Griffin",
+    firstName: "Eric",
+    email: "griffin@gmail.com",
+    phone: "763 234 235",
+    createdAt: "01.01.2023",
+    activities: [
+      {
+        text: "Eric purchased Playstation 5 Digital Edition",
+        time: "3 day ago",
+      },
+      {
+        text: "Eric added 3 items into their wishlist",
+        time: "1 week ago",
+      },
+      {
+        text: "Eric purchased Sony Bravia KD-32w800",
+        time: "2 weeks ago",
+      },
+      {
+        text: "Eric reviewed a product",
+        time: "1 month ago",
+      },
+      {
+        text: "Eric added 1 items into their wishlist",
+        time: "1 month ago",
+      },
+      {
+        text: "Eric reviewed a product",
+        time: "2 months ago",
+      },
+    ],
+  },
+];
+
+export const totalVisit = {
+  title: "Total Visit",
+  color: "#FF8042",
+  dataKey: "visit",
+  chartData: [
+    {
+      name: "Sun",
+      visit: 4000,
+    },
+    {
+      name: "Mon",
+      visit: 3000,
+    },
+    {
+      name: "Tue",
+      visit: 2000,
+    },
+    {
+      name: "Wed",
+      visit: 2780,
+    },
+    {
+      name: "Thu",
+      visit: 1890,
+    },
+    {
+      name: "Fri",
+      visit: 2390,
+    },
+    {
+      name: "Sat",
+      visit: 3490,
+    },
+  ],
+};
+export const revenue = {
+  title: "Profit Earned",
+  color: "#8884d8",
+  dataKey: "profit",
+  chartData: [
+    {
+      name: "Sun",
+      profit: 4000,
+    },
+    {
+      name: "Mon",
+      profit: 3000,
+    },
+    {
+      name: "Tue",
+      profit: 2000,
+    },
+    {
+      name: "Wed",
+      profit: 2780,
+    },
+    {
+      name: "Thu",
+      profit: 1890,
+    },
+    {
+      name: "Fri",
+      profit: 2390,
+    },
+    {
+      name: "Sat",
+      profit: 3490,
+    },
+  ],
+};
+export const devices = [
+  { name: "Mobile", value: 400, color: "#0088FE" },
+  { name: "Desktop", value: 300, color: "#00C49F" },
+  { name: "Laptop", value: 300, color: "#FFBB28" },
+  { name: "Tablet", value: 200, color: "#FF8042" },
+];
+export const revenueanalytics = [
+  {
+    name: "Sun",
+    books: 4000,
+    clothes: 2400,
+    electronic: 2400,
+  },
+  {
+    name: "Mon",
+    books: 3000,
+    clothes: 1398,
+    electronic: 2210,
+  },
+  {
+    name: "Tue",
+    books: 2000,
+    clothes: 9800,
+    electronic: 2290,
+  },
+  {
+    name: "Wed",
+    books: 2780,
+    clothes: 3908,
+    electronic: 2000,
+  },
+  {
+    name: "Thu",
+    books: 1890,
+    clothes: 4800,
+    electronic: 2181,
+  },
+  {
+    name: "Fri",
+    books: 2390,
+    clothes: 3800,
+    electronic: 2500,
+  },
+  {
+    name: "Sat",
+    books: 3490,
+    clothes: 4300,
+    electronic: 2100,
+  },
+];
---
+++ index.js
@@ -0,0 +1,193 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+import jwt from "jsonwebtoken";
+
+import {
+  topdeals,
+  users,
+  totalVisit,
+  revenue,
+  devices,
+  revenueanalytics,
+} from "./data.js";
+
+export let products = [
+  {
+    id: 1,
+    img: "https://store.sony.com.au/on/demandware.static/-/Sites-sony-master-catalog/default/dw1b537bbb/images/PLAYSTATION5W/PLAYSTATION5W.png",
+    title: "Playstation 5 Digital Edition",
+    color: "white",
+    producer: "Sony",
+    price: "$250.99",
+    createdAt: "01.02.2023",
+    inStock: true,
+  },
+  {
+    id: 2,
+    img: "https://www.pngmart.com/files/6/Dell-Laptop-PNG-Image.png",
+    title: "Dell Laptop KR211822",
+    color: "black",
+    producer: "Dell",
+    price: "$499.99",
+    createdAt: "01.02.2023",
+    inStock: true,
+  },
+  {
+    id: 3,
+    img: "http://images.samsung.com/is/image/samsung/uk-led-tv-hg40ed670ck-hg40ed670ckxxu-001-front",
+    title: "Samsung TV 4K SmartTV",
+    color: "gray",
+    producer: "Samsung",
+    price: "$999.49",
+    createdAt: "01.02.2023",
+    inStock: true,
+  },
+  {
+    id: 4,
+    img: "https://raylo.imgix.net/iphone-14-blue.png",
+    title: "Apple Iphone 14 Pro Max",
+    color: "white",
+    producer: "Apple",
+    price: "$799.49",
+    createdAt: "01.02.2023",
+    inStock: true,
+  },
+  {
+    id: 5,
+    img: "https://www.signify.com/b-dam/signify/en-aa/about/news/2020/20200903-movie-night-essentials-popcorn-ice-cream-and-the-new-philips-hue-play-gradient-lightstrip/packaging-lighstrip.png",
+    title: "Philips Hue Play Gradient",
+    color: "rainbow",
+    producer: "Philips",
+    price: "$39.99",
+    createdAt: "01.02.2023",
+  },
+  {
+    id: 6,
+    img: "https://www.smartworld.it/wp-content/uploads/2019/09/High_Resolution_PNG-MX-Master-3-LEFT-GRAPHITE.png",
+    title: "Logitech MX Master 3",
+    color: "black",
+    producer: "Logitech",
+    price: "$59.49",
+    createdAt: "01.02.2023",
+    inStock: true,
+  },
+  {
+    id: 7,
+    img: "https://www.pngarts.com/files/7/Podcast-Mic-PNG-Picture.png",
+    title: "Rode Podcast Microphone",
+    color: "gray",
+    producer: "Rode",
+    price: "$119.49",
+    createdAt: "01.02.2023",
+  },
+  {
+    id: 8,
+    img: "https://5.imimg.com/data5/SW/VM/MY-5774620/toshiba-split-ac-2-ton-3-star-rated-ras-24s3ks-500x500.png",
+    title: "Toshiba Split AC 2",
+    color: "white",
+    producer: "Toshiba",
+    price: "$899.99",
+    createdAt: "01.02.2023",
+    inStock: true,
+  },
+  {
+    id: 9,
+    img: "https://img.productz.com/review_image/102489/preview_sony-kdl-50w800b-50-inch-hdtv-review-superb-picture-102489.png",
+    title: "Sony Bravia KDL-47W805A",
+    color: "black",
+    producer: "Sony",
+    price: "$970.49",
+    createdAt: "01.02.2023",
+  },
+  {
+    id: 10,
+    img: "https://venturebeat.com/wp-content/uploads/2015/07/As_AO1-131_gray_nonglare_win10_03.png?fit=1338%2C1055&strip=all",
+    title: "Acer Laptop 16 KL-4804",
+    color: "black",
+    producer: "Acer",
+    price: "$599.99",
+    createdAt: "01.02.2023",
+    inStock: true,
+  },
+];
+app.use(cors());
+app.use(express.json());
+
+// Dashboard
+app.get("/api/dashboard/topdeals", (req, res) => {
+  res.json(topdeals);
+});
+app.get("/api/dashboard/visit", (req, res) => {
+  res.json(totalVisit);
+});
+app.get("/api/dashboard/revenue", (req, res) => {
+  res.json(revenue);
+});
+app.get("/api/dashboard/devices", (req, res) => {
+  res.json(devices);
+});
+app.get("/api/dashboard/revenueanalytics", (req, res) => {
+  res.json(revenueanalytics);
+});
+
+// GET USERS
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+// GET USER
+app.get("/api/users/:id", (req, res) => {
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+  console.log(user);
+  res.json(user);
+});
+
+// ADD USER
+app.post("/api/users", (req, res) => {
+  users.unshift(req.body);
+  res.json(users);
+});
+
+// DELETE USER
+app.delete("/api/users/:id", (req, res) => {
+  users = users.filter((user) => user.id !== parseInt(req.params.id));
+  res.json("User deleted!");
+});
+
+// GET PRODUCTS
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+// GET PRODUCT
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find(
+    (product) => product.id === parseInt(req.params.id)
+  );
+  res.json(product);
+});
+
+// ADD PRODUCT
+app.post("/api/products", (req, res) => {
+  products.unshift(req.body);
+  res.json(products);
+});
+
+// DELETE PRODUCT
+app.delete("/api/products/:id", (req, res) => {
+  products = products.filter(
+    (product) => product.id !== parseInt(req.params.id)
+  );
+  res.json("Product deleted!");
+});
+
+app.listen(process.env.PORT || 8800, () => {
+  console.log("Connected to backend.");
+});
+app.post("/api/login", (req, res) => {
+  const { email } = req.body;
+  const jwtSecretKey = "P@ssword123";
+  const token = jwt.sign(email, jwtSecretKey);
+  res.json({ message: "Login Successfuly", token });
+});
